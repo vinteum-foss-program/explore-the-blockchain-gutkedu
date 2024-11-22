10f09845afa72d601da719866bbb54203aa6c924
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2024-11-22 11:47:56 -0300

    fix: rpc connect

--- submission/setup.sh
+++ submission/setup.sh
@@ -2,6 +2,6 @@ wget https://bitcoincore.org/bin/bitcoin-core-27.1/bitcoin-27.1-x86_64-linux-gnu
 tar -xzvf bitcoin-27.1-x86_64-linux-gnu.tar.gz
 ln -s $PWD/bitcoin-27.1/bin/* /usr/local/bin/
 mkdir -p ~/.bitcoin
-echo "rpcconnect=RPC_CONNECT" >> ~/.bitcoin/bitcoin.conf
+echo "rpcconnect=84.247.182.145" >> ~/.bitcoin/bitcoin.conf
 echo "rpcuser=classroom" >> ~/.bitcoin/bitcoin.conf
 echo "rpcpassword=R4uo1NgQGuFx" >> ~/.bitcoin/bitcoin.conf
